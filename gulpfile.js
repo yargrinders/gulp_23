7df9e56d79576a1a60e0206d8933d0acd9414237
yargrinders <yargrindersp20@gmail.com>
2023-03-07 17:17:09 +0100

    main

---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+const {src, dest, watch, parallel, series} = require('gulp');
+
+const scss = require('gulp-sass')(require('sass'));
+const concat = require('gulp-concat');
+const uglify = require('gulp-uglify-es').default;
+const browserSync = require('browser-sync').create();
+const autoprefixer = require('gulp-autoprefixer');
+const clean = require('gulp-clean');
+
+// FUNCTIONS
+function scripts() {
+    return src(['app/js/*.js', '!app/js/main.min.js'])
+        .pipe(concat('main.min.js'))
+        .pipe(uglify())
+        .pipe(dest('app/js'))
+        .pipe(browserSync.stream());
+}
+
+// Sass .pipe(scss({ outputStyle: 'compressed'})) - Compressed
+// Sass .pipe(scss()) - NotCompressed
+function styles() {
+    return src(['app/scss/*.scss', 'app/components/*.scss'])
+        .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
+        .pipe(concat('style.min.css'))
+        .pipe(scss({ outputStyle: 'compressed'})) 
+        .pipe(dest('app/css'))
+        .pipe(browserSync.stream());
+}
+
+function watching() {
+    watch(['app/js/main.js'], scripts)
+    watch(['app/scss/*.scss', 'app/components/*.scss'], styles)
+    watch(['app/**/*.html']).on('change', browserSync.reload);
+}
+
+function browsersync() {
+    browserSync.init({
+        server: {
+            baseDir: "app/"
+        }
+    });
+}
+
+function cleanDist(){
+    return src('dist')
+    .pipe(clean())
+}
+
+function building() {
+    return src([
+        'app/css/style.min.css',
+        'app/js/main.min.js',
+        'app/*.html'
+    ], {base : 'app'})
+        .pipe(dest('dist'))
+}
+
+// EXPORTS
+exports.styles = styles;
+exports.scripts = scripts;
+exports.watching = watching;
+exports.browsersync = browsersync;
+exports.cleanDist = cleanDist;
+exports.building = building;
+
+exports.build = series(cleanDist, building);
+exports.default = parallel(styles, scripts, browsersync, watching);
